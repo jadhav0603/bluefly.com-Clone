1882325ed852f9a24dcf4ab59db1a04bd2fcc348
vijay jadhav <jadhavvijay0808@gmail.com>
2025-01-24 16:15:21 +0530

    add routes for getting data in favourite

--- Backend/Routes/addProduct.Route.js
+++ Backend/Routes/addProduct.Route.js
@@ -24,4 +24,21 @@ router.post('/favouriteData',async (req,res)=>{
 })
 
 
+router.get('/getFavouriteData', async(req,res)=>{
+    try {
+        const collectionName = mongoose.connection.db.collection('favourites')
+
+        const data = await collectionName.find().toArray();
+
+        if(!data || data.length === 0){
+            return res.status(404).json({success: false})
+        }
+        res.status(200).json({success:true, data})
+        
+    } catch (error) {
+        res.status(500).json({"/getFavriteData route error": error.message})
+    }
+})
+
+
 module.exports = router
