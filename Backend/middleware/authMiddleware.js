63952c7ae40921c405eeff9ffbff2d1ffe92bcb5
vijay jadhav <jadhavvijay0808@gmail.com>
2025-02-23 18:47:55 +0530

    update favorite routes

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -2,11 +2,13 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const authMiddleware = (req,res,next)=>{
-    const token = req.header('Authorization')
+    const authHeader = req.header('Authorization')
     
-    if(!token){
+    if(!authHeader){
         return res.status(401).json({message:"Access Denied"})
     }
+
+    const token = authHeader.split(" ")[1]
 
     try{
         const decoded = jwt.verify(token,process.env.SECRET_KEY)
