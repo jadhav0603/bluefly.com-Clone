04f96b64853152b03cc78b180299747b90f3ec53
vijay jadhav <jadhavvijay0808@gmail.com>
2025-02-23 17:54:18 +0530

    update favorite routes

--- Backend/Routes/registration.Route.js
+++ Backend/Routes/registration.Route.js
@@ -6,25 +6,25 @@ const router = express.Router()
 const user = require('../models/userModel')
 
 
-router.post('/', async(req, res)=>{
-    const{firstName,lastName,email,password} = req.body
+router.post('/', async (req, res) => {
+    const { firstName, lastName, email, password } = req.body
 
     try {
-        const existedUser = await user.findOne({email});
+        const existedUser = await user.findOne({ email });
 
-        if(existedUser){
-            return res.status(400).json({message:"User Already existed"})
+        if (existedUser) {
+            return res.status(400).json({ message: "User Already existed" })
         }
 
         const hashPass = await bcrypt.hash(password, 10)
 
-        const NewUser = new user({firstName,lastName,email,password:hashPass})
+        const NewUser = new user({ firstName, lastName, email, password: hashPass })
         await NewUser.save()
-        
-        const token = jwt.sign({userID:userExisted._id}, process.env.SECRET_KEY,{expiresIn:"1h"})
-        res.status(201).json({message:"User Register Successfully"}, token, userExisted)
+
+        const token = jwt.sign({ userID: NewUser._id }, process.env.SECRET_KEY, { expiresIn: "1h" });
+        res.status(201).json({ message: "User Register Successfully", token, user: NewUser });
     } catch (error) {
-        res.status(500).json({RegisterError:error.message})
+        res.status(500).json({ RegisterError: error.message })
     }
 })
 
